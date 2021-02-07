1892b94c38c9c5b31109e4799c4611df8c02655a
Samuil Plamenov <samuil.plamenov@harpercollins.co.uk>
2021-02-07 16:53:20 +0000

    Displaying items and adding ability to add new items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 from todo_app.flask_config import Config
+from todo_app.data.session_items import get_items, add_item
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -8,7 +9,16 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    items_list = get_items()
+    return render_template('index.html', items_list=items_list)
+
+
+@app.route('/add_item', methods=['POST'])
+def add_todo_item():
+    item_title = request.form.get('title')
+    add_item(item_title)
+    items_list = get_items()
+    return render_template('index.html', items_list=items_list)
 
 
 if __name__ == '__main__':
