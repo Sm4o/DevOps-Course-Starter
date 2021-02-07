f67b4b21b98079809214666c38aab3f76fefa6a5
Samuil Plamenov <samuil.plamenov@harpercollins.co.uk>
2021-02-07 17:07:54 +0000

    Changing behaviour to add item on top of list instead of bottom
    Adding functionality to mark an item as completed
    
    Adding better styling to form for adding items
    
    Sorting items by status
    
    Adding ability to delete item
    
    Adding message when no items to do

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,7 +1,19 @@
-from flask import Flask, render_template, request
+from flask import (
+    Flask,
+    render_template, 
+    request, 
+    redirect,
+    url_for,
+)
 
 from todo_app.flask_config import Config
-from todo_app.data.session_items import get_items, add_item
+from todo_app.data.session_items import (
+    get_items, 
+    add_item,
+    get_item,
+    save_item,
+    delete_item,
+)
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -17,8 +29,21 @@ def index():
 def add_todo_item():
     item_title = request.form.get('title')
     add_item(item_title)
-    items_list = get_items()
-    return render_template('index.html', items_list=items_list)
+    return redirect(url_for('index'))
+
+
+@app.route('/complete/<item_id>')
+def complete_item(item_id):
+    item = get_item(item_id)
+    item['status'] = 'Completed'
+    save_item(item)
+    return redirect(url_for('index'))
+
+
+@app.route('/delete/<int:item_id>')
+def remove_item(item_id):
+    item = delete_item(item_id)
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
