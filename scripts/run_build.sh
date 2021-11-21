c630f5367b3709e9961da00fe6397ce9061fa23c
Samuil Plamenov <samuil.plamenov@harpercollins.co.uk>
2021-11-21 16:04:24 +0000

    making sure to include travis commit tag

--- scripts/run_build.sh
+++ scripts/run_build.sh
@@ -1,3 +1,4 @@
 docker build --target production --tag sm4o/todo.app:latest .
-docker build --target production --tag sm4o/todo.app:TRAVIS_COMMIT .
+docker build --target production --tag sm4o/todo.app:$TRAVIS_COMMIT .
 docker push sm4o/todo.app:latest
+docker push sm4o/todo.app:$TRAVIS_COMMIT
