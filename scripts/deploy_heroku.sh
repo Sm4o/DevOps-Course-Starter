ffaefd4421e1478dd665623c8e76235fb73f1fe1
Samuil Plamenov <samuil.plamenov@harpercollins.co.uk>
2021-11-22 17:12:26 +0000

    Test deployment from branch

--- scripts/deploy_heroku.sh
+++ scripts/deploy_heroku.sh
@@ -3,6 +3,8 @@ set -e
 
 # Get the latest image from Docker Hub (built by your CI pipeline) 
 docker pull sm4o/todo.app:latest
+# Login Docker to Heroku registry
+docker login --username=_ --password="${HEROKU_API_KEY}" registry.heroku.com
 # Tag it for Heroku
 docker tag sm4o/todo.app:latest registry.heroku.com/todo-app-corndel/web
 # Push it to Heroku registry
