d7f2e45b7d181bd34c522187aa39428dea39b654
Samuil Plamenov <samuil.plamenov@harpercollins.co.uk>
2021-11-22 11:49:56 +0000

    troubleshooting port

--- scripts/deploy_heroku.sh
+++ scripts/deploy_heroku.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
+set -e
+
 # Get the latest image from Docker Hub (built by your CI pipeline) 
 docker pull sm4o/todo.app:latest
 # Tag it for Heroku
 docker tag sm4o/todo.app:latest registry.heroku.com/todo-app-corndel/web
 # Push it to Heroku registry
 docker push registry.heroku.com/todo-app-corndel/web
+
+# Release
+heroku container:release web
