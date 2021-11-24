f48d05646add2f76330051ca26acf9ab75745045
Samuil Plamenov <samuil.plamenov@harpercollins.co.uk>
2021-11-24 12:08:07 +0000

    test with --password-stdin

--- scripts/deploy_heroku.sh
+++ scripts/deploy_heroku.sh
@@ -4,11 +4,11 @@ set -e
 # Get the latest image from Docker Hub (built by your CI pipeline) 
 docker pull sm4o/todo.app:latest
 # Login Docker to Heroku registry
-docker login --username=_ --password="${HEROKU_API_KEY}" registry.heroku.com
+# docker login --username=_ --password="${HEROKU_API_KEY}" registry.heroku.com
 # Tag it for Heroku
 docker tag sm4o/todo.app:latest registry.heroku.com/todo-app-corndel/web
 # Push it to Heroku registry
 docker push registry.heroku.com/todo-app-corndel/web
 
 # Release
-heroku container:release web
+heroku container:release -a web
