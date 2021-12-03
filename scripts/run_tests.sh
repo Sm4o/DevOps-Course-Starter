d22a54cfe3bcf298126d1e66ebe71c3f0b46bd0a
Samuil Plamenov <samuil.plamenov@harpercollins.co.uk>
2021-12-03 00:54:20 +0000

    I think it's fixed

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,3 +1,3 @@
 docker build --target test --tag todo.app:test .
 docker run --env-file .env.test todo.app:test tests
-docker run --env BOARD_ID --env APP_KEY --env TOKEN todo.app:test tests_e2e
+docker run --env MONGODB_CONNECTION --env DATABASE_NAME --env SECRET_KEY --env DATABASE_NAME todo.app:test tests_e2e
